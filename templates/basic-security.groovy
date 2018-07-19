f826c97f0f107caae174b72591941bcd67025c61
Yatish Sharma <yatish1996ss@gmail.com>
2018-07-19 15:01:31 +0530

    Update basic-security.groovy

--- templates/basic-security.groovy
+++ templates/basic-security.groovy
@@ -6,28 +6,19 @@ import jenkins.install.InstallState
 
 
 def instance = Jenkins.getInstance()
-def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-def users = hudsonRealm.getAllUsers()
-users_s = users.collect { it.toString() }
 
-// Create the admin user account if it doesn't already exist.
-if ("{{ jenkins_admin_username }}" in users_s) {
-    println "Admin user already exists - updating password"
 
-    def user = hudson.model.User.get('{{ jenkins_admin_username }}');
-    def password = hudson.security.HudsonPrivateSecurityRealm.Details.fromPlainPassword('{{ jenkins_admin_password }}')
-    user.addProperty(password)
-    user.save()
-}
-else {
-    println "--> creating local admin user"
+if (!instance.isUseSecurity()) {
+    println "--> creating local user 'admin'"
 
+    def hudsonRealm = new HudsonPrivateSecurityRealm(false)
     hudsonRealm.createAccount('{{ jenkins_admin_username }}', '{{ jenkins_admin_password }}')
     instance.setSecurityRealm(hudsonRealm)
 
     def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
     instance.setAuthorizationStrategy(strategy)
     instance.save()
+}
 
 if (!jenkins.installState.isSetupComplete()) {
         InstallState.INITIAL_SETUP_COMPLETED.initializeState()
